c2922469339b938622e78cedcb4e400faa146474
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-09-12 13:57:03 +0000

    sep122024 commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [cnt, setCount] = useState(0);
 
   const inc = () => {
     setCount(c=>c+1); 
   }
   return (
     <div>
-      <p>Button clicked {count} times</p>
+      <p>Button clicked {cnt} times</p>
       <button onClick={inc}>Click me</button>
     </div>
   );
